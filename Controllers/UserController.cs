483dcb5f1338ed9e47ad5856779970574e4fdead
github_cloudlabsuser_1988@cloudlabsaiuser.com <github_cloudlabsuser_1988@cloudlabsaiuser.com>
2024-06-20 15:21:15 +0000

    Deloy stuff

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using CRUD_application_2.Models;
+using System;
 using System.Linq;
 using System.Web.Mvc;
  
@@ -11,18 +12,21 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
         }
  
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            return View();
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
         }
  
         // POST: User/Create
@@ -30,13 +34,16 @@ namespace CRUD_application_2.Controllers
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            userlist.Add(user);
+            return Content("user created");
         }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            return View(Edit(id));
         }
  
         // POST: User/Edit/5
@@ -47,13 +54,32 @@ namespace CRUD_application_2.Controllers
             // It receives user input from the form submission and updates the corresponding user's information in the userlist.
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            // If an error occurs during the process, it returns the Edit view to display any validation errors
+            try
+            {
+                var found = userlist.FindIndex(x => x.Id == id);
+
+                if (found == -1)
+                {
+                    return HttpNotFound();
+                }
+
+                userlist[found] = user;
+
+                return Index();
+            }
+            catch (Exception ex)
+            {
+                return Edit(id);
+            }
+
         }
  
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            return View(Delete(id));
         }
  
         // POST: User/Delete/5
@@ -61,6 +87,9 @@ namespace CRUD_application_2.Controllers
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            userlist.FindIndex(x => x.Id == id);
+
+            return Index();
         }
     }
 }
